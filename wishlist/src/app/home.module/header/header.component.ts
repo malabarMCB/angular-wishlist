446411542373795ced82b2ab33769fe8edfb1c45
Dmitry Grinevich <grinevich97@gmail.com>
2019-11-19 21:26:58 +0200

    Fix header component

--- wishlist/src/app/home.module/header/header.component.ts
+++ wishlist/src/app/home.module/header/header.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Game} from '../../game/game.model';
 import {Store} from '@ngrx/store';
 import {GameState, getGamesInCart} from '../../game/game.reducer';
 import {removeGameFromCart} from '../../game/game.actions';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
   games: Game[];
+
+  private getGamesInCartSubscription: Subscription;
 
   constructor(private store: Store<GameState>) { }
 
@@ -22,5 +25,9 @@ export class HeaderComponent implements OnInit {
   removeGameFromCart(gameId: string): boolean {
     this.store.dispatch(removeGameFromCart({gameId}));
     return false;
+  }
+
+  ngOnDestroy(): void {
+    this.getGamesInCartSubscription.unsubscribe();
   }
 }

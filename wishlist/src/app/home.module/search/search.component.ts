83d876e7abf3b8c2ae6b88f98b71115e45bd080f
Dmitry Grinevich <dmitry.grinevich@idealscorp.com>
2019-11-21 12:19:19 +0200

    Add search to query string param

--- wishlist/src/app/home.module/search/search.component.ts
+++ wishlist/src/app/home.module/search/search.component.ts
@@ -1,19 +1,36 @@
-import { Component} from '@angular/core';
-import {Store} from '@ngrx/store';
-import {GameState} from '../../game/game.reducer';
-import {setSearchValue} from '../../game/game.actions';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { GameState } from '../../game/game.reducer';
+import { setSearchValue } from '../../game/game.actions';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.sass']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  private searchValue: string;
 
-  constructor(private store: Store<GameState>) { }
+  constructor(private store: Store<GameState>, private route: ActivatedRoute, private router: Router) {
+    this.route.queryParams.subscribe(params => this.searchValue = params['search'] || '');
+  }
+
+  ngOnInit() {
+    this.setSearchValue();
+  }
 
   search(searchValue: string): boolean {
-    this.store.dispatch(setSearchValue({searchValue}));
+    const homePagePath = '';
+    const navigationPromise = searchValue
+      ? this.router.navigate([homePagePath], { queryParams: { search: searchValue } })
+      : this.router.navigate([homePagePath]);
+
+    navigationPromise.then(_ => this.setSearchValue());
     return false;
+  }
+
+  private setSearchValue(): void {
+    this.store.dispatch(setSearchValue({ searchValue: this.searchValue }));
   }
 }

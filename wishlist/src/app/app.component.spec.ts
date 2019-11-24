f1e26c509b50afc7f7f2f8b7079f582cdf99c6e1
Dmitry Grinevich <grinevich97@gmail.com>
2019-11-24 23:39:08 +0200

    Fix app-app.component.spec

--- wishlist/src/app/app.component.spec.ts
+++ wishlist/src/app/app.component.spec.ts
@@ -20,16 +20,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'wishlist'`, () => {
+  it(`should have as title 'Wishlist'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('wishlist');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to wishlist!');
+    expect(app.title).toEqual('Wishlist');
   });
 });

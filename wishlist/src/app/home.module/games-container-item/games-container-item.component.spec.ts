bfc1d93c98e1d645394fb7231e122da5a6e7a25a
Dmitry Grinevich <grinevich97@gmail.com>
2019-11-25 22:51:46 +0200

    Remove redundant rests

--- wishlist/src/app/home.module/games-container-item/games-container-item.component.spec.ts
+++ wishlist/src/app/home.module/games-container-item/games-container-item.component.spec.ts
@@ -1,25 +1,2 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { GamesContainerItemComponent } from './games-container-item.component';
-
 describe('GamesContainerItemComponent', () => {
-  let component: GamesContainerItemComponent;
-  let fixture: ComponentFixture<GamesContainerItemComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ GamesContainerItemComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(GamesContainerItemComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
--- wishlist/src/app/home.module/shopping-cart-game/shopping-cart-game.component.spec.ts
+++ wishlist/src/app/home.module/shopping-cart-game/shopping-cart-game.component.spec.ts
@@ -1,25 +1,3 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ShoppingCartGameComponent } from './shopping-cart-game.component';
-
 describe('ShoppingCartGameComponent', () => {
-  let component: ShoppingCartGameComponent;
-  let fixture: ComponentFixture<ShoppingCartGameComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ShoppingCartGameComponent ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ShoppingCartGameComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
